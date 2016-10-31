3eb4e72d2d652556e97974b486841290da3bc926
swebber <scott@teagan.org>
2016-10-30 23:47:36 -0400

    clean up the about page

--- views/about/index.php
+++ views/about/index.php
@@ -1,7 +1,17 @@
-<div class="container theme-showcase" role="main">
+<div class="container">
 
-    <h1>About</h1>
-    <p>Kelly Webber is currently earning her BFA in graphic design from the Maryland Institute College of Art in Baltimore, Maryland, anticipating graduation in May, 2017.</p>
-    <p>Kelly strives to include a handmade element in her work while working efficiently with content and media. Kelly’s work ranges from web design to paper cutting.</p>
+    <h1>ABOUT THE WEDDING</h1>
+    <p class="about-address">
+        <b>THE ANCHOR INN</b><br />
+        7617 WATER OAK POINT ROAD<br />
+        PASADENA, MARYLAND 21122
+    </p>
+    
+    <p class="about-venue">
+        We are getting married at the Anchor Inn on June 16th at 6:00 PM.
+        The ceremony and reception are both taking place at the same venue and
+        will be both indoors and outdoors. For those of you bringing small children please
+        be mindful as the venue is on the water.
+    </p>
 
 </div>
--- views/layout.php
+++ views/layout.php
@@ -24,7 +24,7 @@ $isActive = function ($page) use ($controller) {
     <link href="/assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
-    <link href="/assets/css/theme.css?001" rel="stylesheet">
+    <link href="/assets/css/theme.css?002" rel="stylesheet">
 
 </head>
 <body>
@@ -70,7 +70,6 @@ $isActive = function ($page) use ($controller) {
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
     <script src="/dist/js/bootstrap.min.js"></script>
-    <script src="/assets/js/docs.min.js"></script>
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <script src="/assets/js/ie10-viewport-bug-workaround.js"></script>
 
